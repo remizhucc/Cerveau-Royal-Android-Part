c2f7766398a9163f020441d89290d75173adecc9
wangjiayang <wangjiayang0301@gmail.com>
2019-05-20 15:03:41 +0200

    Add ids profils/friends/startGame

--- app/src/main/java/Activity/IndexActivity.java
+++ app/src/main/java/Activity/IndexActivity.java
@@ -1,6 +1,13 @@
 package Activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.cerveauroyal.R;
 
 public class IndexActivity extends Activity {
+
+
 }
