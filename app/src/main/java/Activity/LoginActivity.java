2026fbe547f4ec622c6ca78b46e08afcf7d7884e
Yixuan-Huang <yixuanhuangyx@gmail.com>
2019-05-23 15:34:06 +0200

    use setOnclickListener to add dynamic content

--- app/src/main/java/Activity/LoginActivity.java
+++ app/src/main/java/Activity/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,13 +16,45 @@ import static helper.AccountHelper.isAuthentic;
 import static helper.AccountHelper.setMyInformationFromServer;
 
 public class LoginActivity extends Activity {
+
+    private String email;
+    private String password;
+
     private EditText emailEditText;
-    private  EditText passwordEditText;
+    private EditText passwordEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        emailEditText = (EditText) findViewById(R.id.email);
+        passwordEditText = (EditText) findViewById(R.id.password);
+
+        Button btnLogIn = (Button) findViewById(R.id.button_login);
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = emailEditText.getText().toString();
+                password = passwordEditText.getText().toString();
+                if(email.isEmpty()||password.isEmpty()) {
+                    try {
+                        if (isAuthentic(email, password)) {
+                            setMyInformationFromServer(email);
+                            Intent intent = new Intent(LoginActivity.this, IndexActivity.class);
+                            startActivity(intent);
+                        } else {
+                            //LENGTH_SHORT 2s LONG 3.5s
+                            Toast.makeText(LoginActivity.this, "Wrong authentication", Toast.LENGTH_SHORT).show();
+                        }
+                    }catch (Exception e){
+                        Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(LoginActivity.this, "Field empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 
