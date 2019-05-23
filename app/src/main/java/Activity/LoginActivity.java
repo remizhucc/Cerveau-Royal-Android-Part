3723c8cfa4908d5a27b7b3b728d766b7faabe13a
Yixuan-Huang <yixuanhuangyx@gmail.com>
2019-05-23 16:00:42 +0200

    test with xiaoroubao

--- app/src/main/java/Activity/LoginActivity.java
+++ app/src/main/java/Activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +18,7 @@ import static helper.AccountHelper.setMyInformationFromServer;
 
 public class LoginActivity extends Activity {
 
+    private static final String TAG = "LoginActivity";
     private EditText emailEditText;
     private EditText passwordEditText;
 
@@ -51,6 +53,7 @@ public class LoginActivity extends Activity {
                 }
             }catch (Exception e){
                 Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                Log.e(TAG,"FailToLogIn:"+e.getMessage());
             }
         }
     }
