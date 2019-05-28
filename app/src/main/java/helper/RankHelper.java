416166d12beff8c9cef1b298ed9077921abd5c10
xiaoroubao1996 <13816257947@163.com>
2019-05-28 17:40:32 +0800

    rank fix bug

--- app/src/main/java/helper/RankHelper.java
+++ app/src/main/java/helper/RankHelper.java
@@ -8,21 +8,21 @@ public class RankHelper {
 
 
     public static int getRankDrawableId(Constant.RANK rank, Activity activity) {
-        String mDrawableName = "@drawable/rank" + rank.toString();
+        String mDrawableName = "@drawable/rank_" + rank.toString().toLowerCase();
         return activity.getResources().getIdentifier(mDrawableName, "drawable", activity.getPackageName());
     }
 
     public static String getRankName(Constant.RANK rank) {
-        switch (rank.toString()) {
-            case "pawn":
+        switch (rank) {
+            case PAWN:
                 return "Pawn";
-            case "knight":
+            case KNIGHT:
                 return "Knight";
-            case "bishop":
+            case BISHOP:
                 return "Bishop";
-            case "tower":
+            case TOWER:
                 return "Tower";
-            case "queen":
+            case QUEEN:
                 return "Queen";
             default:
                 return "NULL";
