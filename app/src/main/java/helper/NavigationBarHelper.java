ab624068e6f12766c8f19af67c3eaab90e79e196
ZeguZegu <18717776983@163.com>
2019-06-07 11:20:17 +0200

    hide ui

---
+++ app/src/main/java/helper/NavigationBarHelper.java
@@ -0,0 +1,19 @@
+package helper;
+
+import android.app.Activity;
+import android.view.View;
+
+public class NavigationBarHelper {
+    public static void hideSystemUI(Activity activity) {
+        // Set the IMMERSIVE flag.
+        // Set the content to appear under the system bars so that the content
+        // doesn't resize when the system bars hide and show.
+        activity.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+}
